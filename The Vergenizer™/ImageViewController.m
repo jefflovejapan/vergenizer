c65b2af67f9e104e7554e581ff3201858a873c44
Jeff Blagdon <jeffblagdon@gmail.com>
2014-02-01 21:54:02 -0500

    Added double-tap gesture and zooming for IVC

--- The Vergenizer™/ImageViewController.m
+++ The Vergenizer™/ImageViewController.m
@@ -15,10 +15,35 @@
 
 @implementation ImageViewController
 
-#define MAX_ZOOM_SCALE 2.0
+#define MAX_ZOOM_SCALE 1.0
 #define MIN_ZOOM_SCALE 0.1
 
-- (void)viewWillAppear:(BOOL)animated{
+#pragma actions
+
+
+- (IBAction)zoomDoubleTap:(id)sender {
+    CGPoint tapLocation = [sender locationInView:[self viewForZoomingInScrollView:self.scrollView]];
+    if (self.scrollView.zoomScale < self.scrollView.maximumZoomScale) {
+        CGSize currentRectSize = self.scrollView.bounds.size;
+        CGRect zoomRect = CGRectMake(tapLocation.x - (currentRectSize.width / 2.0), tapLocation.y - (currentRectSize.height / 2.0), (currentRectSize.width / 2.0), (currentRectSize.height / 2.0));
+        [self.scrollView zoomToRect:zoomRect animated:YES];
+    } else {
+        [self.scrollView zoomToRect:self.imageView.bounds animated:YES
+         ];
+    }
+}
+
+
+#pragma lifecycle methods
+
+- (void)viewDidLoad{
+    [self.scrollView addSubview:self.imageView];
+    self.scrollView.maximumZoomScale = MAX_ZOOM_SCALE;
+    self.scrollView.minimumZoomScale = MIN_ZOOM_SCALE;
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [self resetImage];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,23 +52,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
-    return self.imageView;
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation{
+
 }
 
-- (UIImageView *)imageView{
-    if (!_imageView) {
-        _imageView = [[UIImageView alloc]initWithFrame:CGRectZero];
-    }
-    return _imageView;
-}
-
-- (void)viewDidLoad{
-    [self.scrollView addSubview:self.imageView];
-    self.scrollView.maximumZoomScale = MAX_ZOOM_SCALE;
-    self.scrollView.minimumZoomScale = MIN_ZOOM_SCALE;
-    [self resetImage];
-}
+#pragma helper methods
 
 - (void)resetImage{
     if (self.scrollView) {
@@ -58,6 +71,19 @@
             [self.scrollView setNeedsDisplay];
         }
     }
+}
+
+#pragma instantiation
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return self.imageView;
+}
+
+- (UIImageView *)imageView{
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc]initWithFrame:CGRectZero];
+    }
+    return _imageView;
 }
 
 @end
